d724e6af23b3f161f826a3969d6d0b39c1311493
Shreyans13 <shreyans1313@gmail.com>
2021-03-27 17:14:56 +0530

    Map View fixed

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,9 @@
 <template>
-	<l-map
-		class="nodes-map"
-		:zoom="zoom"
-		:center="center"
-		style="height: 100%; width: 100%"
-	>
-		<l-tile-layer :url="url"></l-tile-layer>
-	</l-map>
+	<v-img :aspect-ratio="1 / 2" width="100%">
+		<l-map :zoom="zoom" :center="center" style="height: 100%; width: 100%">
+			<l-tile-layer :url="url"></l-tile-layer>
+		</l-map>
+	</v-img>
 </template>
 
 <script>
@@ -14,7 +11,7 @@ import { latLng } from "leaflet";
 import { LMap, LTileLayer } from "vue2-leaflet";
 
 export default {
-	name: "MobileMap",
+	name: "Map",
 	components: {
 		LMap,
 		LTileLayer,
@@ -22,10 +19,11 @@ export default {
 	data: () => ({
 		lat: 22.804565,
 		long: 86.202873,
-		zoom: 13,
+		zoom: 11,
 		center: latLng(22.804565, 86.202873),
 		url:
 			"https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png",
+		// url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 	}),
 };
 </script>
--- src/views/OrderDelivery.vue
+++ src/views/OrderDelivery.vue
@@ -1,5 +1,5 @@
 <template>
-	<div style="height:100%">
+	<div style="height: 100%">
 		<v-app-bar app fixed flat elevate-on-scroll scroll color="#1e1e1e">
 			<v-btn icon @click="backNavigation">
 				<v-icon>fas fa-arrow-left</v-icon>
@@ -7,11 +7,21 @@
 			<span>Your Order Details</span>
 		</v-app-bar>
 		<Map />
+		<v-footer dark padless app fixed>
+			<v-card class="flex" flat tile>
+				<v-card-title class="justify-center">
+					<strong>
+						Your order will be delivered sooon
+					</strong>
+				</v-card-title>
+			</v-card>
+		</v-footer>
 	</div>
 </template>
 
 <script>
 import Map from "../components/Map.vue";
+import router from "../router/index";
 
 export default {
 	name: "OrderDelivery",
@@ -19,5 +29,10 @@ export default {
 		Map,
 	},
 	data: () => ({}),
+	methods: {
+		backNavigation() {
+			router.go(-1);
+		},
+	},
 };
 </script>
