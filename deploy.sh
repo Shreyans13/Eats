2c79f8f869dc25158659717323421ca545dfae82
Shreyans13 <shreyans1313@gmail.com>
2021-04-24 03:52:08 +0530

    Bug fixes

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'test-run'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
