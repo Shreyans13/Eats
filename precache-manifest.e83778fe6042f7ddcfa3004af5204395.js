44dab0d416dd65b31c93155470d5a5ddaa935308
Shreyans_13 <shreyans.jain@juspay.in>
2021-07-15 02:33:53 +0530

    🚀 deploy

---
+++ precache-manifest.e83778fe6042f7ddcfa3004af5204395.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b5455542324b5e2468",
+    "url": "/Eats/css/account.56017e01.css"
+  },
+  {
+    "revision": "77635748c1a995aa6c8d",
+    "url": "/Eats/css/account~confirmorder~history~menu~ordersummary.1f74ea8e.css"
+  },
+  {
+    "revision": "e86ba1e58812dfbbbc88",
+    "url": "/Eats/css/app.d840e006.css"
+  },
+  {
+    "revision": "a7f182007c155c19fbf2",
+    "url": "/Eats/css/bookresturant.a3c46487.css"
+  },
+  {
+    "revision": "6affb876867b71a89029",
+    "url": "/Eats/css/bookresturant~menu.d77a986e.css"
+  },
+  {
+    "revision": "70fb1b212843e9d9e96a",
+    "url": "/Eats/css/chunk-vendors.97892fd0.css"
+  },
+  {
+    "revision": "30150e741e06a7b53671",
+    "url": "/Eats/css/confirmorder.a3ebaf9b.css"
+  },
+  {
+    "revision": "3a116a4c9985196f40ff",
+    "url": "/Eats/css/confirmorder~history~ordersummary.80a98ae1.css"
+  },
+  {
+    "revision": "501f7fb6aec91df3e761",
+    "url": "/Eats/css/history.c309eb75.css"
+  },
+  {
+    "revision": "4a31066113ce836a2533",
+    "url": "/Eats/css/menu.8e7eb10c.css"
+  },
+  {
+    "revision": "0d74541ecb2b4e922a65",
+    "url": "/Eats/css/orderdelivery.a3ebaf9b.css"
+  },
+  {
+    "revision": "894b2334bfef64a42405",
+    "url": "/Eats/css/register.c7af056b.css"
+  },
+  {
+    "revision": "28d63b9ada96c023ae8586b57b3a34f8",
+    "url": "/Eats/img/eats.28d63b9a.png"
+  },
+  {
+    "revision": "55b1934114446b8a9e8e0cc0843033a5",
+    "url": "/Eats/index.html"
+  },
+  {
+    "revision": "a2b5455542324b5e2468",
+    "url": "/Eats/js/account.ae1743a5.js"
+  },
+  {
+    "revision": "77635748c1a995aa6c8d",
+    "url": "/Eats/js/account~confirmorder~history~menu~ordersummary.b1bd7372.js"
+  },
+  {
+    "revision": "e86ba1e58812dfbbbc88",
+    "url": "/Eats/js/app.f941901c.js"
+  },
+  {
+    "revision": "a7f182007c155c19fbf2",
+    "url": "/Eats/js/bookresturant.7ccf62b8.js"
+  },
+  {
+    "revision": "6affb876867b71a89029",
+    "url": "/Eats/js/bookresturant~menu.dca5e1b6.js"
+  },
+  {
+    "revision": "70fb1b212843e9d9e96a",
+    "url": "/Eats/js/chunk-vendors.e0f98344.js"
+  },
+  {
+    "revision": "30150e741e06a7b53671",
+    "url": "/Eats/js/confirmorder.e1cd7e9b.js"
+  },
+  {
+    "revision": "3a116a4c9985196f40ff",
+    "url": "/Eats/js/confirmorder~history~ordersummary.c52debd0.js"
+  },
+  {
+    "revision": "8d563fc65b9c0fc09f63",
+    "url": "/Eats/js/goout.7504ef98.js"
+  },
+  {
+    "revision": "501f7fb6aec91df3e761",
+    "url": "/Eats/js/history.0cd8fef2.js"
+  },
+  {
+    "revision": "4a31066113ce836a2533",
+    "url": "/Eats/js/menu.3a68b791.js"
+  },
+  {
+    "revision": "0d74541ecb2b4e922a65",
+    "url": "/Eats/js/orderdelivery.496d3a98.js"
+  },
+  {
+    "revision": "6baf3b709987c986cc3b",
+    "url": "/Eats/js/ordersummary.5638d993.js"
+  },
+  {
+    "revision": "894b2334bfef64a42405",
+    "url": "/Eats/js/register.dc108f9f.js"
+  },
+  {
+    "revision": "cf4002c8543c512655012b914ce0b4bc",
+    "url": "/Eats/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Eats/robots.txt"
+  }
+]);
