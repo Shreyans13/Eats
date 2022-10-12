cb170e066aecebd57f243a3aa9e2010db354e596
Vincent Ho <vinmaster99@gmail.com>
2022-10-11 22:24:52 -0700

    add backend api with expressjs

--- src/api/index.js
+++ src/api/index.js
@@ -1,67 +1,57 @@
-const resturants = require("../fakeData/resturants").default.resturants;
-const resturantsDetail = require("../fakeData/resturants").default
-	.resturantsDetail;
+import axios from 'axios';
 
-let history = [];
+const BASE_URL = 'http://localhost:8000';
 
-const getResturants = () => {
-	return new Promise((resolve) => {
-		setTimeout(function() {
-			resolve(resturants);
-		}, 1000);
-	});
+const getResturants = async () => {
+  return (await axios.get(`${BASE_URL}/resturants`)).data;
 };
 
-const getResturantsDetail = (id) => {
-	return new Promise((resolve) => {
-		setTimeout(function() {
-			resolve({
-				data: resturantsDetail.data.find((el) => el.id == id),
-			});
-		}, 1000);
-	});
+const getResturantsDetail = async id => {
+  return (await axios.get(`${BASE_URL}/resturants/${id}`)).data;
 };
 
-const getHistory = () => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(history);
-		}, 1000);
-	});
+const getHistory = async () => {
+  return (await axios.get(`${BASE_URL}/history`)).data;
 };
 
-const postOrder = (order) => {
-	console.log("OrderPosted");
-	console.log(order);
-	history.push(order);
-	console.log(history);
+const postOrder = async order => {
+  console.log(order);
+  return (await axios.post(`${BASE_URL}/order`, order)).data;
 };
 
-const bookTable = (details) => {
-	console.log("TableBooked");
-	console.log(details);
+const bookTable = details => {
+  console.log('TableBooked');
+  console.log(details);
 };
 
-const cancelTable = (details) => {
-	console.log("TableCanceled");
-	console.log(details);
+const cancelTable = details => {
+  console.log('TableCanceled');
+  console.log(details);
 };
 
-const signUpUser = user => {
-	console.log("User Signed Up")
-	console.log(user)
-}
+const userAccount = async () => {
+  return (await axios.get(`${BASE_URL}/account`, { withCredentials: true })).data;
+};
 
-const logout = () => {
-	console.log("Loged Out")
-}
+const signUpUser = async user => {
+  user = (await axios.post(`${BASE_URL}/sign-up`, user, { withCredentials: true })).data;
+  console.log(user);
+  console.log('User Signed Up');
+  return user;
+};
+
+const logout = async () => {
+  await axios.post(`${BASE_URL}/logout`);
+  console.log('Loged Out');
+};
 export default {
-	getResturantsDetail,
-	getResturants,
-	getHistory,
-	postOrder,
-	bookTable,
-	cancelTable,
-	signUpUser,
-	logout
+  getResturantsDetail,
+  getResturants,
+  getHistory,
+  postOrder,
+  bookTable,
+  cancelTable,
+  userAccount,
+  signUpUser,
+  logout,
 };
